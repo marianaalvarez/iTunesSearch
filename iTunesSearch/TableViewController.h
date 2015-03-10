8ec6bafc238de5264ae34f049036f5cff87b5b80
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-03-10 10:00:18 -0300

    Alteraçoes nos comentarios.

--- iTunesSearch/TableViewController.h
+++ iTunesSearch/TableViewController.h
@@ -3,7 +3,7 @@
 //  iTunesSearch
 //
 //  Created by joaquim on 09/03/15.
-//  Copyright (c) 2015 joaquim. All rights reserved.
+//  Copyright (c) 2015 Mariana Alvarez. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
