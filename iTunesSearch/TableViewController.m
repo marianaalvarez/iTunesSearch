483214ee3bf48fb8fb0ad3ecfe6be9f457de2958
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-03-10 10:47:19 -0300

    Adiçao de novas informaçoes na célula.

--- iTunesSearch/TableViewController.m
+++ iTunesSearch/TableViewController.m
@@ -56,6 +56,8 @@
     
     [celula.nome setText:filme.nome];
     [celula.tipo setText:@"Filme"];
+    [celula.genero setText:filme.genero];
+    [celula.artista setText:filme.artista];
     
     return celula;
 }
